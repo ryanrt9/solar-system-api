9e83105f390008bfc2afd7b7fb519e41eea45326
Valentina Ruiz <v.zruiz06@gmail.com>
2022-10-21 08:38:43 -0700

    started planets class

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,10 @@
+from unicodedata import name
 from flask import Blueprint
+
+class Planet:
+    def __init__(self, id, name, description, moon_count):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moon_count = moon_count
 
