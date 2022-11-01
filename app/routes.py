7251748555475c8afad7adb89fdd34ee564901e0
Valentina Ruiz <v.zruiz06@gmail.com>
2022-11-01 07:41:50 -0700

    adding new files

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,22 @@
 from unicodedata import name
 from flask import Blueprint, jsonify
 
-class Planet:
-    def __init__(self, id, name, description, moon_count):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.moon_count = moon_count
+# class Planet:
+#     def __init__(self, id, name, description, moon_count):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.moon_count = moon_count
 
-planets = [Planet(1, "Mercury", "Closest to the sun", 0), 
-Planet(2, "Venus", "Planet of love", 0),
-Planet(3, "Earth", "Third rock from the sun", 1),
-Planet(4, "Mars", "Red planet", 2),
-Planet(5, "Jupiter", "The Gas Giant", 57),
-Planet(6, "Saturn", "Astroid Rings", 63),
-Planet(7, "Uranus", "empty", 27),
-Planet(1, "Neptune", "Can't see with naked eye", 14) 
-]     
+# planets = [Planet(1, "Mercury", "Closest to the sun", 0), 
+# Planet(2, "Venus", "Planet of love", 0),
+# Planet(3, "Earth", "Third rock from the sun", 1),
+# Planet(4, "Mars", "Red planet", 2),
+# Planet(5, "Jupiter", "The Gas Giant", 57),
+# Planet(6, "Saturn", "Astroid Rings", 63),
+# Planet(7, "Uranus", "empty", 27),
+# Planet(1, "Neptune", "Can't see with naked eye", 14) 
+# ]
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
